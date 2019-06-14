052586bdfc4198b4917bd174097539bbf74634c9
Steven Mai <dev@gossipcoin.net>
2019-06-14 11:01:09 +0200

    equalize with development repo

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc9
 $(package)_patches=fix_aarch64_build.patch
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  patch -p1 < $($(package)_patch_dir)/fix_aarch64_build.patch
 endef
 
 define $(package)_set_vars
